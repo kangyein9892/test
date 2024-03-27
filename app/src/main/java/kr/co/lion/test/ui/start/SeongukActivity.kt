4df72142a01df1b5214f2d1950f3c8dabf6f6074
seongukHeo <heoseonguk3@gmail.com>
2024-03-27 16:38:21 +0900

    #7 (Feat) Activity 생성

---
+++ app/src/main/java/kr/co/lion/test/ui/start/SeongukActivity.kt
@@ -0,0 +1,16 @@
+package kr.co.lion.test.ui.start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.lion.test.R
+
+class SeongukActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seonguk)
+    }
+
+    private fun seongUk(){
+        val name = "허성욱"
+    }
+}
