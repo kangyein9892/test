75242e4cda9c2b2ec4fbfb83755108e3b6d5c276
yein <inwon9892@naver.com>
2024-03-27 14:06:21 +0900

    [Feat] #3 로그인 버튼 클릭 기능 구현

--- app/src/main/java/kr/co/lion/test/ui/home/HomeFragment.kt
+++ app/src/main/java/kr/co/lion/test/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kr.co.lion.test.R
+import kr.co.lion.test.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -13,6 +14,9 @@ import kr.co.lion.test.R
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -25,7 +29,11 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+
+        binding.button.setOnClickListener {
+
+        }
     }
 }
